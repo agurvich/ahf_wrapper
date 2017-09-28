6d94ef7b346b5dd9c868bd160935f66aee559397
zhafen_stampede <zachary.h.hafen@gmail.com>
2017-09-27 22:02:15 -0500

    Started updating the way AHF_halos_add files are generated, just hooking it in to the other work I did.

--- get_ahf_halos_adds.py
+++ get_ahf_halos_adds.py
@@ -18,11 +18,19 @@ import galaxy_diver.read_data.ahf as read_ahf
 # Get the directory the AHF data is in
 sdir = sys.argv[1]
 
-# Get the directory where the snapshot times are located
-metafile_dir = sys.argv[2]
-
 # Get the snapshots to process the AHF halo files for
-snum = int( sys.argv[3] )
+snum = int( sys.argv[2] )
+
+# Get the directory where the snapshot times are located
+metafile_dir = sys.argv[3]
+
+radii_mass_fractions = [ 0.5, 0.75, 0.99 ]
+
+simulation_data_dir = sys.argv[4]
+
+galaxy_cut = float( sys.argv[5] )
+
+length_scale = sys.argv[6]
 
 ########################################################################
 # Perform the calculation.
